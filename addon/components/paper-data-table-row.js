f89538819b441534158c42f2cca7ad87ad260b0b
Ian Barrick <ianbarrick@gmail.com>
2016-08-16 13:20:02 -0400

    added click pointer

--- addon/components/paper-data-table-row.js
+++ addon/components/paper-data-table-row.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 import layout from '../templates/components/paper-data-table-row';
 
+const { computed } = Ember;
+
 export default Ember.Component.extend({
 	layout,
 	tagName: 'tr',
 	classNames: ['md-row'],
 	showEdit: false,
+	attributeBindings: ['style'],
+	style: computed('edit', 'onClick', function() {
+		if (this.get('onClick') || this.get('edit')) {
+			return "cursor: pointer;";
+		} else {
+			return "";
+		}
+	}),
 	click() {
 		this.sendAction('onClick');
 		if (this.get('edit')) {
