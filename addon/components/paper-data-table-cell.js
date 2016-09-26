d8ef63cac4c594deb7a5976a7b9eaab5567730af
Ian Barrick <ianbarrick@gmail.com>
2016-09-25 21:31:09 -0400

    allowed for setting the width of columns

--- addon/components/paper-data-table-cell.js
+++ addon/components/paper-data-table-cell.js
@@ -1,13 +1,25 @@
 import Ember from 'ember';
 import layout from '../templates/components/paper-data-table-cell';
 
+const { computed, Handlebars } = Ember;
+
 export default Ember.Component.extend({
 	layout,
 	tagName: 'td',
 	classNameBindings: ['numeric:md-numeric','edit:md-clickable'],
+	attributeBindings: ['style'],
 	classNames: ['md-cell'],
 	edit: false,
 	showEdit: false,
+	width: null,
+	style: computed('width', function() {
+		let width = Handlebars.Utils.escapeExpression(this.get('width'));
+		if (width) {
+			return Handlebars.SafeString(`width: ${width}px;`);
+		} else {
+			return undefined;
+		}
+	}),
 	click() {
 		this.sendAction('onClick');
 		this.set('showEdit',true);
--- addon/components/paper-data-table-column.js
+++ addon/components/paper-data-table-column.js
@@ -1,13 +1,24 @@
 import Ember from 'ember';
 import layout from '../templates/components/paper-data-table-column';
 
+const { computed, Handlebars } = Ember;
+
 export default Ember.Component.extend({
 	layout,
 	tagName: 'th',
 	classNameBindings: ['numeric:md-numeric','active:md-active','sortProp:md-sort'],
+	attributeBindings: ['style'],
 	classNames: ['md-column'],
 	active: false,
 	currentProp: null,
+	style: computed('width', function() {
+		let width = Handlebars.Utils.escapeExpression(this.get('width'));
+		if (width) {
+			return Handlebars.SafeString(`width: ${width}px;`);
+		} else {
+			return undefined;
+		}
+	}),
 	sortProp: null,
 	sortDir: null,
 	click() {
