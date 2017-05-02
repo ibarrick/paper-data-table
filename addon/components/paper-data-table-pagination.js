cbadbf2fcd10f98f70b1a7c9838ebd35c3a5fec0
Anthony Pessy <anthony.pessy@gmail.com>
2017-05-02 10:45:51 +0200

    cap endOffset to total when available

--- addon/components/paper-data-table-pagination.js
+++ addon/components/paper-data-table-pagination.js
@@ -13,7 +13,9 @@ export default Component.extend({
 	startOffset: computed('page', 'limit', function() {
 		return Math.max((this.get('page') - 1) * this.get('limit') + 1, 1); // 1-based index
 	}),
-	endOffset: computed('startOffset', 'limit', function() {
-		return this.get('startOffset') + this.get('limit');
+	endOffset: computed('startOffset', 'limit', 'total', function() {
+	  let endOffset = this.get('startOffset') + this.get('limit');
+	  let total = this.get('total');
+		return total ? Math.min(endOffset, total) : endOffset;
 	})
 });
